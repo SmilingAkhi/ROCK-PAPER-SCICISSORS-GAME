8eeff9864e3a3710f6ae12b24dc3c77d7e7ad7e5
Abdulrahman <bhabdulrahaman@gmail.com>
2022-05-08 11:17:04 +0100

    final commit

--- game.js
+++ game.js
@@ -1,80 +1,93 @@
 "use script";
 console.log("hello world");
 
-//trying out how to return a random string from an array
-
+let computerScore;
+let playerScore;
+let computerSelection;
+let playerSelection;
 function computerPlay() {
   const randomOutput = ["rock", "paper", "scissors"];
 
   const computer = Math.floor(Math.random() * 3);
   const answ = randomOutput[computer];
-
   return answ;
 }
-let computerSelection;
-let playerSelection;
-let computerScore;
-let playerScore;
+// computerSelection = computerPlay();
+// console.log(computerSelection);
+// playerSelection = playerOption();
+// console.log(playerSelection);
 
 game();
 function game() {
   computerScore = 0;
   playerScore = 0;
-  for (i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     computerSelection = computerPlay();
     console.log(computerSelection);
-    playerOption();
+
+    playerSelection = playerOption();
+    console.log(playerSelection);
+
+    console.log(`computer: ${computerSelection}
+    you: ${playerSelection}`);
+    playRound(playerSelection, computerSelection);
   }
+
   console.log(declareWinner());
   function declareWinner() {
-    if (playerScore === computerScore) {
-      alert(`${playerScore} - ${computerScore} \nTie Game`);
+    if (computerScore === playerScore) {
+      console.log(`${playerScore} - ${computerScore} \nTie Game`);
       return `${playerScore} - ${computerScore} \nTie Game`;
     } else if (playerScore > computerScore) {
-      alert(`${playerScore} - ${computerScore} \nYou Win`);
+      console.log(`${playerScore} - ${computerScore} \nYou Win`);
       return `${playerScore} - ${computerScore} \nYou win`;
     } else {
-      alert(
+      console.log(
         `${playerScore} - ${computerScore} \nYou Lost, Better luck Next time!`
       );
       return `${playerScore} - ${computerScore} \nYou Lost, Better luck Next time!`;
     }
   }
 }
-function playerOption() {
-  playerSelection = prompt("Rock,paper or scissors");
-  console.log(playerSelection);
 
-  if (
-    playerSelection.toLowerCase() == "rock" ||
-    playerSelection.toLowerCase() == "paper" ||
-    playerSelection.toLowerCase() == "scissors"
-  ) {
-    console.log(playRound(playerSelection, computerSelection));
-  } else {
-    alert("That's not a Valid Choice");
-    playerOption();
-  }
+function playerOption() {
+  choices = prompt("Rock,paper or scissors");
+  validateOption();
+  // console.log(choices);
+  return choices;
 }
 
-computerSelection = computerPlay();
-console.log(computerSelection);
-playerSelection = playerOption();
+function validateOption() {
+  if (
+    choices.toLowerCase() == "rock" ||
+    choices.toLowerCase() == "paper" ||
+    choices.toLowerCase() == "scissors"
+  ) {
+    return choices;
+  } else alert("input a value");
+  playerOption();
+}
 
 function playRound(playerSelection, computerSelection) {
-  // your code here!
-  computerSelection = computerPlay().toLowerCase();
-  playerSelection = playerSelection.toLowerCase();
-  if (computerSelection == playerSelection) {
-    alert("it's a tie");
-    return "it's a tie";
+  if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
+    console.log("tie game");
+    return console.log(
+      "computer Score:" + computerScore + "\nYour Score:" + playerScore
+    );
   } else if (
-    (computerSelection == "paper" && playerSelection == "rock") ||
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper")
+    (computerSelection.toLowerCase() === "rock" &&
+      playerSelection.toLowerCase() === "scissors") ||
+    (computerSelection.toLowerCase() === "scissors" &&
+      playerSelection.toLowerCase() === "paper") ||
+    (computerSelection.toLowerCase() === "paper" &&
+      playerSelection.toLowerCase() === "rock")
   ) {
-    alert("you lose");
-    return "You lose!!, paper beat rock";
-  } else alert("you win");
-  return "you WIN!! scissors beats paper";
+    console.log(`you lost! ${computerSelection} beats ${playerSelection}`);
+    return console.log(
+      "computer Score:" + ++computerScore + "\nYour Score:" + playerScore
+    );
+  } else console.log(`you win! ${playerSelection} beats ${computerSelection}`);
+  return console.log(
+    "computer Score:" + computerScore + "\nYour Score:" + ++playerScore
+  );
 }
